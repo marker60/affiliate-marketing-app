f6b0205b4ac8e3330572f1bb7b2b3d1b98cce696
marker60 <southernmgd@gmail.com>
2025-10-06 18:56:16 -0500

    stabilize: deps + css config + theme + markdown + brief route

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", // <-- enables dark mode via a CSS class
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
